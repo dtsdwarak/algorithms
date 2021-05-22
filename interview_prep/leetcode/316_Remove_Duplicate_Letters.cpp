c2645fc58b1cd06717d5d4da2c59ad7b398e8cdc
Dwaraka Nath <dtsdwarak@hotmail.com>
2021-05-22 13:07:04 +0530

    Add more stack problems

---
+++ interview_prep/leetcode/316_Remove_Duplicate_Letters.cpp
@@ -0,0 +1,53 @@
+/*
+* Reference - 
+* https://buttercola.blogspot.com/2016/01/leetcode-remove-duplicate-letters.html
+*/
+class Solution {
+public:
+    
+    int firstCharacterOccurrence(unordered_map<char, int> &lastIndexMap) {
+        
+        int minIndex = INT_MAX;
+        
+        for(auto i: lastIndexMap)
+            minIndex = min(minIndex, i.second);
+        
+        return minIndex;
+    }
+    
+    
+    string removeDuplicateLetters(string s) {
+        
+        unordered_map<char, int> lastIndexMap;
+        for(int i=0; i<s.length(); i++) {
+            lastIndexMap[s[i]] = i;
+        }
+        
+        int startIndex = 0;
+        string ans;
+        
+        while(!lastIndexMap.empty()) {
+            
+            int minIndex = firstCharacterOccurrence(lastIndexMap);
+            
+            char minCharacter = 'z' + 1;
+            int minCharacterIndex = INT_MAX;
+            
+            for(int i=startIndex; i<=minIndex; i++) {
+                if (minCharacter > s[i] && lastIndexMap.find(s[i]) != lastIndexMap.end()) {
+                    minCharacter = s[i];
+                    minCharacterIndex = i;
+                }
+            }
+            
+            ans += minCharacter;
+            
+            lastIndexMap.erase(minCharacter);
+            startIndex = minCharacterIndex + 1;
+            
+        }
+        
+        return ans;
+        
+    }
+};
